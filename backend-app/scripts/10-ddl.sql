bd3f3e92717468fb460232bff3c68290ca28874f
solsin <solasin@gmail.com>
2025-08-23 10:34:32 +0900

    init db

---
+++ backend-app/scripts/10-ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE memo
+(
+  id                 SERIAL PRIMARY KEY,
+  title              VARCHAR(100) NOT NULL,
+  memo               VARCHAR(1000) NOT NULL,
+  create_date        TIMESTAMP    NOT NULL,
+  update_date        TIMESTAMP NULL
+);
---
+++ backend-app/scripts/20-init-data.sql
@@ -0,0 +1,1 @@
+insert into memo (title, memo, create_date) values('test', 'test memo', CURRENT_DATE);
